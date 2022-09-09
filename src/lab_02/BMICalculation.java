ff0d4be776d430ff80d4c2d5b5cf11f042b2917d
Thanh.Tran <thanh.tran@waverleysoftware.com>
2022-09-09 22:35:04 +0700

    update BMICalculation code

--- src/lab_02/BMICalculation.java
+++ src/lab_02/BMICalculation.java
@@ -8,6 +8,7 @@ public class BMICalculation {
         float yourHeight;
         float yourWeight;
         float yourBMI;
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your height (meter): ");
         yourHeight = scanner.nextFloat();
@@ -16,5 +17,15 @@ public class BMICalculation {
         //calculate BMI
         yourBMI = yourWeight / (yourHeight * 2);
         System.out.println("Your BMI is: " + yourBMI);
+
+        if(yourBMI < 18.5) {
+            System.out.println("You are underweight");
+        } else if (yourBMI >= 18.5 && yourBMI <= 24.9) {
+            System.out.println("You are normal weight");
+        } else if (yourBMI > 24.9 && yourBMI <= 29.9) {
+            System.out.println("You are overweight");
+        } else {
+            System.out.println("You are obesity");
+        }
     }
 }
